b1d0905e390ec8cada243ce2c5ac53bf12533876
yuvraj-117 <71701495+yuvraj-117@users.noreply.github.com>
2021-03-22 16:26:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0;
+noseY=0;
+
+
+
+
+
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/MTBmFQfn/haloooo.png');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function take_snapshot(){
+    save('myfilterimg.png');
+    
+}
+function modelLoaded(){
+    console.log('poseNet is intialized');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+         noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y+10;
+    }
+}
+function draw(){
+    image(video,0,0,300,300)
+    image(clown_nose,noseX,noseY,30,30);
+    }
+    
